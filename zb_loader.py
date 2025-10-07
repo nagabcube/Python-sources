33bb4ec0934318a1ef5b103551d6b0231a586560
nagabcube <ngabor2013@gmail.com>
2025-10-07 11:21:38 +0200

    Update zb_loader.py

--- zb_loader.py
+++ zb_loader.py
@@ -51,7 +51,7 @@ def getFileList(remote_dir):
 	entries  = []
 
 	try:
-		f = FTP('ftp.ovf.hu', 'vitukih.modellad.ftp', 'Vm20150207')
+		f = FTP('ftp.ovf.hu', 'ftp-user', 'ftp-pwd')
 		f.cwd(remote_dir)
 		f.dir(entries.append)
 	except Exception, msg :
@@ -503,4 +503,5 @@ db2dss('Balaton','tp',int(delta))
 db2dss('Zala','tp',0)
 
 logger.info('**** VEGE ****')
+
 
